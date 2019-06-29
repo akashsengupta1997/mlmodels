b291c5f82081f1c1d54c501b16f6d11c41e3d80b
Akash Sengupta <akash.sengupta1997@gmail.com>
2019-06-29 17:41:03 +0100

    added marginal likelihood computation to bayesian linear regression

--- supervised/bayesian_linear_regression.py
+++ supervised/bayesian_linear_regression.py
@@ -8,6 +8,10 @@ class BayesianLinearRegressor():
     Gaussian likelihood) - allowing for exact inference.
     """
     def __init__(self, prior_precision, noise_var):
+        """
+        :param prior_precision: reciprocal of Gaussian prior variance
+        :param noise_var: Gaussian noise variance (for likelihood)
+        """
         self.prior_precision = prior_precision
         self.noise_var = noise_var
 
@@ -43,11 +47,38 @@ class BayesianLinearRegressor():
 
         return predictive_mean, predictive_cov
 
+    def compute_log_marginal_likelihood(self, X, y):
+        """
+
+        :param X:
+        :param y:
+        :return:
+        """
+        X = self.ones_for_bias_trick(X)
+        ml_cov = (1.0/self.prior_precision) * np.matmul(X, X.T) + self.noise_var * np.eye(X.shape[0])
+        log_ml = self.log_gaussian_pdf(np.zeros(X.shape[0]), ml_cov, y)
+        return log_ml
+
+    def log_gaussian_pdf(self, mean, cov, x):
+        """
+
+        :param mean:
+        :param cov:
+        :param x:
+        :return:
+        """
+        norm_constant = np.sqrt(np.linalg.det(2*np.pi*cov))
+        exp_term = -0.5 * np.dot((x + mean).T, np.dot(np.linalg.inv(cov), (x + mean)))
+        log_prob = np.log(1.0/norm_constant) + exp_term
+        return log_prob
+
     def visualise_line(self, X, y):
         assert len(self.posterior_mean) == 2, "Can only visualise 1D inputs currently :("
         axes = plt.gca()
         plt.scatter(X[:, -1], y)
         low, high = axes.get_xlim()
+        low = low - 5
+        high = high + 5
         x_vals = np.linspace(low, high, 50)
         mean_vals, var_vals = zip(*list(map(self.compute_predictive_distribution, x_vals)))
         mean_vals = np.array(mean_vals)
@@ -57,4 +88,8 @@ class BayesianLinearRegressor():
         plt.plot(x_vals, mean_vals, '-', color='r')
         plt.plot(x_vals, one_std_above_vals, '--', color='gray')
         plt.plot(x_vals, one_std_below_vals, '--', color='gray')
+        plt.text(0.75, 0.1, "Posterior Means: \n Intercept: {} \n Slope:{}".format(
+            round(self.posterior_mean[0], 2),
+            round(self.posterior_mean[1], 2)),
+                 transform=axes.transAxes)
         plt.show()
